4f9699ad82aff9e7fc2d4e07460f7fa47df177ee
koichi.sakamoto <sakamoto0321@gmail.com>
2021-08-16 18:50:42 +0900

    supported test.describe.serial

--- src/test/suite/playwright-editor-support.test.ts
+++ src/test/suite/playwright-editor-support.test.ts
@@ -10,6 +10,7 @@ describe('playwright-editor-support', () => {
 		rootDir = vscode.workspace.workspaceFolders[0] && vscode.workspace.workspaceFolders[0].uri;
 	}
   const testcodes = parse(rootDir.fsPath+'/parsetest/test.js');
+  const testcodes2 = parse(rootDir.fsPath+'/tests/mainpackage.spec.js');
 
   it('should find line 1', () => {
     const hit = findTestCode(testcodes, 1);
@@ -55,5 +56,41 @@ describe('playwright-editor-support', () => {
     const hit = findTestCode(testcodes, 50);
     assert.deepStrictEqual(hit && hit.fullname,'.add(%i, %i) returns ${i}');
   });
+
+  it('mainpackage.spec.js should find line 3', () => {
+    const hit = findTestCode(testcodes2, 3);
+    assert.deepStrictEqual(hit && hit.fullname,'basic test');
+  });
+
+  it('mainpackage.spec.js should find line 9', () => {
+    const hit = findTestCode(testcodes2, 9);
+    assert.deepStrictEqual(hit && hit.fullname,'describe 01');
+  });
+  
+  it('mainpackage.spec.js should find line 10', () => {
+    const hit = findTestCode(testcodes2, 10);
+    assert.deepStrictEqual(hit && hit.fullname,'describe 01 runs first 01');
+  });
+  
+  it('mainpackage.spec.js should find line 11', () => {
+    const hit = findTestCode(testcodes2, 11);
+    assert.deepStrictEqual(hit && hit.fullname,'describe 01 runs second 01');
+  });
+
+  it('mainpackage.spec.js should find line 14', () => {
+    const hit = findTestCode(testcodes2, 14);
+    assert.deepStrictEqual(hit && hit.fullname,'describe 02');
+  });
+  
+  it('mainpackage.spec.js should find line 15', () => {
+    const hit = findTestCode(testcodes2, 15);
+    assert.deepStrictEqual(hit && hit.fullname,'describe 02 runs first 02');
+  });
+  
+  it('mainpackage.spec.js should find line 16', () => {
+    const hit = findTestCode(testcodes2, 16);
+    assert.deepStrictEqual(hit && hit.fullname,'describe 02 runs second 02');
+  });
+
   
 });
