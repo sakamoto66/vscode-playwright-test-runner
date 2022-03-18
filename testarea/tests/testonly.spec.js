d0b38fece8c59efce7d5c16d1ff788e8b6ae9573
koichi.sakamoto <sakamoto0321@gmail.com>
2022-03-19 00:23:53 +0900

    update test code

--- testarea/tests/testonly.spec.js
+++ testarea/tests/testonly.spec.js
@@ -1,19 +1,19 @@
 const { test, expect } = require('@playwright/test');
 
-test.only('basic test', async ({ page }) => {
+test.only('only test 01', async ({ page }) => {
 });
 
-test.describe.only('describe 02', () => {
-  test.only('basic test', async ({ page }) => {
+test.describe.only('only 02', () => {
+  test.only('only test 02', async ({ page }) => {
   });
 });
 
-test.describe.serial.only('describe 02', () => {
-  test.only('basic test', async ({ page }) => {
+test.describe.serial.only('only 03', () => {
+  test.only('only test 03', async ({ page }) => {
   });
 });
 
-test.describe.parallel.only('describe 02', () => {
-  test.only('basic test', async ({ page }) => {
+test.describe.parallel.only('only 04', () => {
+  test.only('only test 04', async ({ page }) => {
   });
 });
